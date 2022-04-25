21b65c7ebbbfe8e06c960690988e8d7154489ead
DESKTOP-2RTHKI5\Булатов Михаил <mikhail-bulatov@bk.ru>
2022-04-25 14:20:42 +0500

    Init

---
+++ AntDesign.Testing/Program.cs
@@ -0,0 +1,41 @@
+﻿using AntDesign.Testing.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddTransient(sp => new HttpClient()
+{
+    DefaultRequestHeaders =
+    {
+        // Use to call the github API on server side
+        {"User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36 Edg/81.0.416.68"}
+    }
+});
+builder.Services.AddAntDesign();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
